f2c18a77810f91ccab8060dd2f8cd3750a244421
Gabreak <100302431+Gabreak@users.noreply.github.com>
2022-09-26 17:24:22 +0600

    Ga

--- Assets/InternalAssets/Game/Core/Menu/GameMenu.cs
+++ Assets/InternalAssets/Game/Core/Menu/GameMenu.cs
@@ -11,6 +11,22 @@ public class GameMenu : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             _menuGame.SetActive(!_menuGame.activeSelf);
+            if (_menuGame.activeSelf)
+            {
+                Time.timeScale = 0;
+                ControlSystemProperties.DisableInvoke();
+            }
+            else
+            {
+                Time.timeScale = 1;
+                ControlSystemProperties.EnableInvoke();
+            }
         }
+    }
+
+    public void GamePlay()
+    {
+        Time.timeScale = 1;
+        ControlSystemProperties.EnableInvoke();
     }
 }
