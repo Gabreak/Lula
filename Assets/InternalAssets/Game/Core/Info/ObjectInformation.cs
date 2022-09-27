09dff504529e2a194f12509619f34882feaf3e31
Gabreak <100302431+Gabreak@users.noreply.github.com>
2022-09-27 13:43:00 +0600

    Ga

--- Assets/InternalAssets/Game/Core/Info/ObjectInformation.cs
+++ Assets/InternalAssets/Game/Core/Info/ObjectInformation.cs
@@ -6,18 +6,18 @@ using UnityEngine.EventSystems;
 
 public class ObjectInformation : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public string TextInfo { get; set; } = "";
+    public string TextInfo { get; set; } = " ";
 
 
-    private void Start() => MouseManager.Instance.TextInfo("");
+    private void Start() => MouseManager.Instance.TextInfo(" ");
 
     private void OnMouseEnter() => MouseManager.Instance.TextInfo(TextInfo);
 
 
-    private void OnMouseExit() => MouseManager.Instance.TextInfo("");
+    private void OnMouseExit() => MouseManager.Instance.TextInfo(" ");
 
     public void OnPointerEnter(PointerEventData eventData) => MouseManager.Instance.TextInfo(TextInfo);
 
-    public void OnPointerExit(PointerEventData eventData) => MouseManager.Instance.TextInfo("");
+    public void OnPointerExit(PointerEventData eventData) => MouseManager.Instance.TextInfo(" ");
 
 }
