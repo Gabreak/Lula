897a2cae35c66d99e4789f9370d17f2dcf3ad02a
Gabreak <100302431+Gabreak@users.noreply.github.com>
2022-09-29 13:23:36 +0600

    Ga

--- Assets/InternalAssets/Game/Core/Room/RoomGirlController.cs
+++ Assets/InternalAssets/Game/Core/Room/RoomGirlController.cs
@@ -1,6 +1,3 @@
-using JetBrains.Annotations;
-
-using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -9,7 +6,6 @@ using UnityEngine;
 public class RoomGirlController : MonoBehaviour
 {
     public int IdBinary = 0;
-    [SerializeField] private int _indexGirl;
     [SerializeField] private GameObject _check;
     private bool _isActive = false;
 
@@ -19,19 +15,18 @@ public class RoomGirlController : MonoBehaviour
         _roomGirls = transform.root.GetComponent<RoomGirls>();
         _check.SetActive(false);
     }
-
     private void OnMouseDown()
     {
         _isActive = !_isActive;
-
         if (_isActive)
+        {
+            _check.SetActive(true);
             _roomGirls.IdBinary += IdBinary;
+        }
         else
+        {
+            _check.SetActive(false);
             _roomGirls.IdBinary -= IdBinary;
-
-        _roomGirls._gameBed[_indexGirl].Check = Convert.ToInt32(_isActive);
-        _roomGirls.UpdateBed(_indexGirl);
-
-        _check.SetActive(_isActive);
+        }
     }
 }
