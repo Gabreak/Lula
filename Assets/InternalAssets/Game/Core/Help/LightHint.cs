f1a2e15971dd35558b5ccf1e81a645791ff39068
Gabreak <100302431+Gabreak@users.noreply.github.com>
2022-10-20 13:28:49 +0600

    Ga

--- Assets/InternalAssets/Game/Core/Help/LightHint.cs
+++ Assets/InternalAssets/Game/Core/Help/LightHint.cs
@@ -3,16 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Localization;
+using UnityEngine.Rendering.PostProcessing;
 
 public class LightHint : MonoBehaviour
 {
-    [SerializeField] private Light _light;
+    [SerializeField] private PostProcessVolume _post;
     [SerializeField] private ObjectHelp[] _objects;
 
     private void Start()
     {
         _objects[0].Sprite.material = _objects[0].MaterialLight;
-        _light.color = Color.black; 
+        //_light.color = Color.black; 
     }
 }
 
